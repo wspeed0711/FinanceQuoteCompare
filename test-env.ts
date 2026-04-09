01151398a0db013a2c8d1901a7c75c0a8626293f
wspeed0711 <victorwoo.wxd@gmail.com>
2026-04-09 17:34:26 +0800

    refactor: Remove IPv4 DNS preference setting
    The `dns.setDefaultResultOrder('ipv4first')` setting has been removed as it's no longer necessary and can potentially mask underlying network configuration issues. The added logging in `test-env.ts` will help debug proxy issues if they re-emerge.

---
+++ test-env.ts
@@ -0,0 +1,4 @@
+console.log("HTTP_PROXY:", process.env.HTTP_PROXY);
+console.log("HTTPS_PROXY:", process.env.HTTPS_PROXY);
+console.log("http_proxy:", process.env.http_proxy);
+console.log("https_proxy:", process.env.https_proxy);
